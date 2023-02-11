8c3fef7a3c3899a207d5d919f86bb02b1f856ddb
Bettina Staartjes <bbstaars@gmail.com>
2023-02-11 15:47:50 -0500

    removed client and server out of develop

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,38 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+//declare varaiable with const 
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+//provide insturctions on webpacks entry and output; set it to equal empty object
 module.exports = () => {
   return {
     mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
+    entry: 'Develop/client/src/js/index.js',
+    install: './src/js/install.js'
+    }
+    //do i need to change output file name??
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      })
+    ]
 
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ]
+    }
   };
 };
